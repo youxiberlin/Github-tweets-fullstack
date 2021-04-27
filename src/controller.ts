b763be87dd0728270bbdb2ccf2666f5eb9158c01
Yuki Sato <yuki.sato@lightcurve.io>
2021-04-27 10:03:01 +0200

    Update tweet message

--- src/controller.ts
+++ src/controller.ts
@@ -4,13 +4,18 @@ import twitterClient from './services/twitter';
 
 export const postHook: RequestHandler = async (req, res) => {
   try {
-    const message = req.body.commits[0].message;
+    const body = req.body;
+    const message = body.commits[0].message;
     const doc = new Message({ message });
     await doc.save();
-    await twitterClient.post('statuses/update', { status: message });
     res.send({
       message: `${req.body.message} stored`
     });
+
+    const tweetMessage = `New commits at ${body.repository.name}\n
+    🚀 ${body.head_commit.message} by ${body.head_commit.committer.username}\n
+    See more details at ${body.compare}`;
+    await twitterClient.post('statuses/update', { status: tweetMessage });
   } catch (e) {
     console.log(e);
   }
