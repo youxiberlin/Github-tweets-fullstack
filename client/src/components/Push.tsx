11ae31df890dd707f278c4488da7c92d87c53898
Yuki Sato <yuki.sato@lightcurve.io>
2021-04-27 20:16:44 +0200

    Add and use moment

--- client/src/components/Push.tsx
+++ client/src/components/Push.tsx
@@ -1,12 +1,14 @@
 import { FunctionComponent } from 'react';
 import Commit from './Commit';
 import { IPush } from '../models/push.model';
+import moment from 'moment';
+moment.locale();
 
 const Push: FunctionComponent<{ push: IPush }> = ({ push }) => {
   return (
     <div>
       <div>
-        {push.pushed_at}
+        {moment(+push.pushed_at * 1000).format('HH:mm DD-MM-YY')}
       </div>
       <div>
         <div>commits</div>
