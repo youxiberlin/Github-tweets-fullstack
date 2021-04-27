68c57e98684854ef7e072d925bbd59b28dab4eb5
Yuki Sato <yuki.sato@lightcurve.io>
2021-04-27 18:47:07 +0200

    Add cors to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,13 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from 'express';
 import { json } from 'body-parser';
+import cors from 'cors';
 import * as database from './services/database';
 import routes from './routes';
 
 const app = express();
 app.use(json());
+app.use(cors());
 app.use('/test', (req, res) => res.status(200).send('hello world'));
 app.use('/api', routes);
 
