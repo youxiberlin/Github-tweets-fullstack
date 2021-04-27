434a9caada7ffca1e202f64e741f20422f361ff4
Yuki Sato <yuki.sato@lightcurve.io>
2021-04-27 09:43:48 +0200

    Add twitter client

--- src/controller.ts
+++ src/controller.ts
@@ -1,11 +1,13 @@
 import { RequestHandler } from 'express';
 import Message from './models/commit';
+import twitterClient from './services/twitter';
 
 export const postHook: RequestHandler = async (req, res) => {
   try {
     const message = req.body.commits[0].message;
     const doc = new Message({ message });
-    await doc.save()
+    await doc.save();
+    await twitterClient.post('statuses/update', { status: message });
     res.send({
       message: `${req.body.message} stored`
     });
