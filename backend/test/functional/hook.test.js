80521324682a62c6c2715792e59a9739badbf824
Yuki Sato <yuki.sato@lightcurve.io>
2021-04-28 19:52:39 +0200

    Add test data and test cases

--- backend/test/functional/hook.test.js
+++ backend/test/functional/hook.test.js
@@ -1,37 +1,56 @@
-const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../../dist/app');
 const request = supertest(app);
 const dbHandler = require('../db-handler');
-const { createPush } = require('../../dist/services/createPush');
+const { createPush } = require('../../dist/services/push');
+const testData = require('../fixtures/testData.json');
 
 beforeAll(async () => await dbHandler.connect());
 afterEach(async () => await dbHandler.clearDatabase());
 afterAll(async () => await dbHandler.closeDatabase());
 
-const pushExample = {
-  pushed_at: 1619548312,
-  compare: 'https://github.com/youxiberlin/Github-tweets-fullstack/compare/2c2e8e4801e7...11ae31df890d',
-  commits: [
-    {
-      message: 'new commit',
-      url: 'https://github.com/youxiberlin/Github-tweets-fullstack/commit/010afa7cef0bc9f07508cd5873a55aa06b6f198c',
-      committer: { username: 'yukisato' }
-    }
-  ]
-}
+describe('test', () => {
+  it('returns "Hello World"', async done => {
+    const res = await request.get('/test');
+    expect(res.status).toBe(200);
+    expect(res.body.message).toBe('hello world');
+    done()
+  })
+});
 
-it('Gets the test endpoint', async done => {
-  const res = await request.get('/test');
-  expect(res.status).toBe(200);
-  expect(res.body.message).toBe('hello world');
-  done()
-})
+describe('findPushes tests', () => {
+  beforeEach(async () => {
+    testData.forEach(async data => {
+      await createPush(data);
+    });
+  });
 
-describe('push', () => {
-  it('can be created correctly', async () => {
-    expect(async () => await createPush(pushExample))
-        .not
-        .toThrow();
+  it('should return all pushes', async () => {
+    const res = await request.get('/api/pushes');
+    expect(res.status).toBe(200);
+    expect(res.body.data.length).toBe(3);
+    res.body.data.forEach(item => {
+      expect(item).toHaveProperty('pushed_at', 'compare', 'commits');
+      item.commits.forEach(commit => {
+        expect(commit).toHaveProperty('message', 'url', 'committer');
+      })
+    });
+  })
+});
+
+describe('Hook functional tests', () => {
+  it('should sccessfully create a new push', () => {
+    const newPush = {
+      pushed_at: 1619551400,
+      compare: "https://github.com/youxiberlin/Github-tweets-fullstack/compare/46c31f1e294c...0d1f024aeded",
+      commits: [{
+        message : "Updated hook controller",
+        url : "https://github.com/youxiberlin/Github-tweets-fullstack/commit/e331c4b2ae78b72fba0bec86f8b727d6bb01c790",
+        committer : "youxiberlin"
+      }]
+    };
+    expect(async () => await createPush(newPush))
+      .not
+      .toThrow()
   });
 });
